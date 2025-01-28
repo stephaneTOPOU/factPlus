c3fabcb8a339441c03d24c3ca18e54b0c3c631c8
Vivien <vivientopou57@gmail.com>
2025-01-28 11:10:52 +0000

    commit

---
+++ app/Http/Controllers/PaiementController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paiements;
+use Illuminate\Http\Request;
+
+class PaiementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Paiements  $paiements
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Paiements $paiements)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Paiements  $paiements
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Paiements $paiements)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Paiements  $paiements
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Paiements $paiements)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Paiements  $paiements
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Paiements $paiements)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProformatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proformats;
+use Illuminate\Http\Request;
+
+class ProformatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Proformats  $proformats
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Proformats $proformats)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Proformats  $proformats
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Proformats $proformats)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Proformats  $proformats
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Proformats $proformats)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Proformats  $proformats
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Proformats $proformats)
+    {
+        //
+    }
+}
